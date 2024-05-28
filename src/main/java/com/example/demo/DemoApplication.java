16e5019c3c27c6bfb8c5c7511e79d4a99475f0fa
916-Serbanoiu-Stefania <91834791+916-Serbanoiu-Stefania@users.noreply.github.com>
2024-05-28 13:34:43 +0300

    modified print statements

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -11,8 +11,11 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		System.out.println("Given array :");
 		List<Integer> givenArray = List.of(1, 2, 3, 4, 3, 2, 1, 1, 2, 3, 1);
+		System.out.println(givenArray);
 		AnimalArray animalArray = new AnimalArray(givenArray);
+		System.out.println("Modified array: ");
 		List<Integer> newArray = animalArray.beFriends();
 		System.out.println(newArray);
 	}
